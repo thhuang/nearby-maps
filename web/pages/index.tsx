325dc90f4ceb7b4f61a27d8147300ce363db657a
TH Huang <tzhsn.huang@gmail.com>
2021-07-04 02:03:06 +0800

    rwd for landing page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Landing() {
   return (<>
     <div className={styles.heroSection}>
       <div className={styles.heroText}>
-        <h1 className="main-headline">原來最棒的咖啡聽就在身邊！</h1> {/* I18N_CAFE_LANDING_HEADER */}
+        <h1 className="main-headline">最棒的咖啡廳就在身邊！</h1> {/* I18N_CAFE_LANDING_HEADER */}
         <p className="sub-headline">
           悠閑的午後，靜謐的夜晚，無論是與自己獨處，還是三五好友相聚，沒什麼比來到一間舒適且充滿特色的咖啡廳更適合的了。讓我們一同來探索這些隱身於巷弄中的神秘空間，享受單純而愜意的美好時光。{/* I18N_CAFE_LANDING_DESCRIPTION */}
         </p>
